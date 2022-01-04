1df1dcbfed728fb187f6b631e7911a020d54a1c1
dax <dax97@live.com.mx>
2022-01-04 17:54:53 -0600

    Se agregaron los demas componentes a la carpeta junto con su funcionalidad basica los componentes son, TodoSearch.js, Todolist.js, TodoItem.js, CreateTodoButton.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
 //import './App.css';
 
 const todos = [
   { text: 'Cortar Cebolla', completed: false },
   { text: 'Tomar el curso de intro a React', completed: false },
-  { text: 'Llorar con la llorona', completed: false }
+  { text: 'Llorar con la llorona', completed: false },
+  { text: 'Lalalala', completed: false }
 ]
 function App(porps) {
   return (
     <React.Fragment> {/* = <>*/}
       <TodoCounter />
 
-      {/*<TodoSearch />*/}
-        <input placeholder="Cebolla" />
-      {/*<TodoList>
-        {todos.map(todo => (
-          <TodoItem />
-        ))}                                                                       
-      </TodoList>*/}
+      <TodoSearch />
 
-      {/*<CreateTodoButton />*/}
+      <TodoList>
+        {todos.map(todo => (
+          <TodoItem key={todo.text} text={todo.text}/>
+        ))}                                                                       
+      </TodoList>
+
+      <CreateTodoButton />
     </React.Fragment>  /*= </>*/
   );
 }
