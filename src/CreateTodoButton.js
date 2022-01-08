8af4584cf2d96b6a8d650441d255298d4d01ca69
dax <dax97@live.com.mx>
2022-01-07 19:14:48 -0600

    se ha creado todas las fucnionalidades principales necesarias de este proyecto con las cuales se obtiene o se arroja informaciÃon mediante eventos en CreateCTodoButton.js TodoItem.js TodoSearch.js

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -3,8 +3,17 @@ import './CreateTodoButton.css';
 
 function CreateTodoButton(props)
 {
+    const onClickButton = (msg) => {
+        alert(msg);
+    }
+
     return (
-        <button className="CreateTodoButton">+</button>
+        <button 
+        className="CreateTodoButton"
+        onClick={() => {onClickButton('Aquí debería abrir un modal')}}
+        >
+            +
+        </button>
     );
 }
 
