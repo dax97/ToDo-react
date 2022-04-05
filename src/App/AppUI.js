52decf33b29f79ba1438a02c247b0f6b6456230b
dax <dax97@live.com.mx>
2022-04-05 12:30:38 -0500

    Se ha establecido la base de la funcionalidad de carga y error mediante efecto

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -6,6 +6,8 @@ import { TodoItem } from '../TodoItem';
 import { CreateTodoButton } from '../CreateTodoButton';
 
 function AppUI({
+    error,
+    loading,
     totalTodos,
     completedTodos,
     searchValue,
@@ -27,6 +29,10 @@ function AppUI({
         />
 
         <TodoList>
+            {error && <p>Desespérate, hubo un error..</p>}
+            {loading && <p>Estamos cargando, no desesperes...</p>}
+            {(!loading && !searchedTodos.length) && <p>¡Crear tu primer TODO!</p>}
+
             {searchedTodos.map(todo => (
                 <TodoItem 
                     key={todo.text} 
